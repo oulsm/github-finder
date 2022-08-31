14898c93f32f39bdb32720203b62e4a01d1c1571
oulsm <s1252701@ouhk.edu.hk>
2022-08-31 13:19:31 +0800

    13_19_31_8_22_update
    Alert Component(bugs)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 import Home from './page/Home';
 import About from './page/About';
 import NotFound from './page/NotFound';
 import {GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
+
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
     <div className='flex flex-col justify-between h-screen'>
       <Navbar/>
       <main className='container mx-auto px-3 pb-12'>
+        <Alert />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import {useState, useContext} from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
+
 function UserSearch() {
     const [text, setText] = useState('')
     
     const {users, searchUsers, clearUsers} = useContext(GithubContext)
+
+    const {setAlert} = useContext(AlertContext) 
 
     const handleChange = (e) => setText(e.target.value)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         if(text === ''){
-            alert('Please enter somthing')
+            setAlert('Please enter somthing','error')
 
         }else{
             searchUsers(text)
