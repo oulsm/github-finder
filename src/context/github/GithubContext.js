a110fdde665f1b15cae01fb28c133a5bf50acd82
oulsm <s1252701@ouhk.edu.hk>
2022-08-31 12:40:28 +0800

    12_40_31_8_22_update
     Clear Users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -37,11 +37,16 @@ export const GithubProvider = ({children}) =>{
             
         )
     }
+
+    const clearUsers = () => dispatch({ type:'CLEAR_USERS'})
+
+
     const setLoading = () => dispatch({ type:'SET_LOADING'})
     return <GithubContext.Provider value={{
         users: state.users,
         loading :state.loading,
         searchUsers,
+        clearUsers,
     }}>
         {children}
     </GithubContext.Provider>
