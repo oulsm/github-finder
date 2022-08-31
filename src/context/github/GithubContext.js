be99ea27f62d52613d0c3250e5166451561cbe5b
oulsm <s1252701@ouhk.edu.hk>
2022-08-31 12:34:48 +0800

    12_34_31_8_22_update
    Search Users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -15,19 +15,24 @@ export const GithubProvider = ({children}) =>{
     }
     const [state, dispatch]= useReducer(githubReducer,initialState )
 
-    const fetchUsers = async () => {
+    const searchUsers = async (text) => {
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`, {
+
+        const params = new URLSearchParams({
+            q:text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
         })
-        const data = await response.json()
+        const {items} = await response.json()
        // setUsers(data);
        // setLoading(false)
         dispatch({
             type:'GET_USERS',
-            payload: data,
+            payload: items,
         }
             
         )
@@ -36,7 +41,7 @@ export const GithubProvider = ({children}) =>{
     return <GithubContext.Provider value={{
         users: state.users,
         loading :state.loading,
-        fetchUsers,
+        searchUsers,
     }}>
         {children}
     </GithubContext.Provider>
